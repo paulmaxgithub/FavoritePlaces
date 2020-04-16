b701028deaf321c903ff54b7758c928be9fd4303
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-17 00:52:17 +0300

    Cell Customizing: 1. new Elements for cell was created in storyboard: imageView, labels for place info (cell properties by default are no longer used). 2. class CustomTableViewCell was created  as model of cell. 3. redefined new properties for cell in class MainViewController.

---
+++ MyFavoritePlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyFavoritePlaces
+//
+//  Created by Paul Max on 4/17/20.
+//  Copyright © 2020 PaulmaX. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
--- MyFavoritePlaces/MainViewController.swift
+++ MyFavoritePlaces/MainViewController.swift
@@ -27,11 +27,10 @@ class MainViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = restaurantNames[indexPath.row]
-        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
-        cell.imageView?.layer.cornerRadius = cell.frame.height / 2
-        cell.imageView?.clipsToBounds = true
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
+        cell.nameLabel.text = restaurantNames[indexPath.row]
+        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.height / 2
         return cell
     }
     
