f39cd45706688ae7a13c2a9ccb41b64b33b0c5f1
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-17 00:17:32 +0300

    Design App (work with size of objects, images): 1. launch screen was created (image +). 2. to assets directory, icons was added. 3. cell customizing (height, cornerRadius for imageView).

--- MyFavoritePlaces/MainViewController.swift
+++ MyFavoritePlaces/MainViewController.swift
@@ -29,8 +29,14 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
-        
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.imageView?.layer.cornerRadius = cell.frame.height / 2
+        cell.imageView?.clipsToBounds = true
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 77
     }
     
     /*
