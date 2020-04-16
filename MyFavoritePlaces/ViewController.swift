1880509fab00014f804cc6dee968042519fa15e6
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-16 19:26:35 +0300

    Initial Commit

---
+++ MyFavoritePlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Paul Max on 4/16/20.
+//  Copyright © 2020 PaulmaX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
