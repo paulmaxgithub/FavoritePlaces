8f7536e8203c28cac8ea11cb83f0f56909e3a006
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-16 20:27:56 +0300

    Initial Project Settings: 1. created new tableView with navigation controller (viewController was deleted) 2. created new file MainViewController.swift (ViewController.swift file was deleted) 3. in class MainViewController, methods for creating and setting up a cell were created. 4. added list of names of places in the [Array] form.

---
+++ MyFavoritePlaces/MainViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MainViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Paul Max on 4/16/20.
+//  Copyright © 2020 PaulmaX. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITableViewController {
+    
+    let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+                           "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+                           "Speak Easy", "Morris Pub", "Вкусные истории",
+                           "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        
+        return cell
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
