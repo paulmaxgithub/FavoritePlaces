09fd0479c1500db7c87d07dce9e7334e888649be
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-22 00:25:08 +0300

    Customize TableView in section adding a new place: 1. created new class for NewPlaceViewController. 2. method to hide the keyboard by tap outside TextField. 3. method to hide the keyboard by click "done" button.

---
+++ MyFavoritePlaces/NewPlaceViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NewPlaceViewController.swift
+//  MyFavoritePlaces
+//
+//  Created by Paul Max on 4/22/20.
+//  Copyright © 2020 PaulmaX. All rights reserved.
+//
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.tableFooterView = UIView()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            
+        } else {
+            view.endEditing(true)
+        }
+    }
+}
+
+extension NewPlaceViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
