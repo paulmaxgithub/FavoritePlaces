379887922f740f515ae22c66e31d77bcecd482f0
Pavel Maximenko <ipaulmax@icloud.com>
2020-04-23 15:09:21 +0300

    Prepare For Sorting: 1. images uploaded to assets catalog (for sorting); 2. TableViewController replaced on a ViewController (implementation of segmentedControl); 3. constraints and other;

--- MyFavoritePlaces/MainViewController.swift
+++ MyFavoritePlaces/MainViewController.swift
@@ -9,9 +9,10 @@
 import UIKit
 import RealmSwift
 
-class MainViewController: UITableViewController {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     var places: Results<Place>!
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +21,11 @@ class MainViewController: UITableViewController {
     }
     
     // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return places.isEmpty ? 0 : places.count
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! CustomTableViewCell
         let place = places[indexPath.row]
         cell.nameLabel.text = place.name
@@ -36,7 +37,7 @@ class MainViewController: UITableViewController {
     }
     
     // MARK: - Table View Delegate
-    override func tableView(_ tableView: UITableView,
+    func tableView(_ tableView: UITableView,
                             commit editingStyle: UITableViewCell.EditingStyle,
                             forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
